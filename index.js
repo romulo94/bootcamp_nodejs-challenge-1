14cc8ad171892fa273b67c9aec0c96949ed81954
Romulo Rocha <romulorocha063@gmail.com>
2019-04-23 12:46:12 -0300

    alteração de sintaxe

--- index.js
+++ index.js
@@ -3,35 +3,37 @@ const nunjucks = require('nunjucks')
 
 const app = express()
 
-app.use(express.urlencoded({ extended: false }))
-
-app.set('view engine', 'njk')
-
 nunjucks.configure('views', {
   autoescape: true,
   express: app,
   watch: true
 })
 
+app.use(express.urlencoded({ extended: false }))
+app.set('view engine', 'njk')
+
 const verificationParams = (req, res, next) => {
-  return req.query.age === undefined ? res.redirect('/') : next()
+  const { age } = req.query
+  return !age ? res.redirect('/') : next()
 }
 
 app.get('/', (req, res) => {
   return res.render('root')
 })
 app.post('/check', (req, res) => {
-  const age = req.body.age
-  return age >= 18
-    ? res.redirect(`/major?age=${age}`)
-    : res.redirect(`/minor?age=${age}`)
+  const { age } = req.body
+
+  return res.redirect(`/${age >= 18 ? 'major' : 'minor'}?age=${age}`)
 })
+
 app.get('/major', verificationParams, (req, res) => {
-  const age = req.query.age
+  const { age } = req.query
+
   return res.render('major', { age })
 })
 app.get('/minor', verificationParams, (req, res) => {
-  const age = req.query.age
+  const { age } = req.query
+
   return res.render('minor', { age })
 })
 
